626c535dee89bea12911b8f9dfa1e85eb7d243ac
msynko <marina.synko@gmail.com>
2019-02-01 15:02:15 -0500

    add JS and finish css

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusSlides(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > x.length) {slideIndex = +1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" active", "");
+   }
+  x[slideIndex-1].style.display = "block";
+   dots[slideIndex-1].className += " active";
+
+}
